915c5cc0484a0ead0542c4b3f459313ce902bde4
Philipp Hug <philipp@hug.cx>
2020-08-24 17:58:24 +0200

    drop old code

--- pkg/pdf-sign/util.go
+++ pkg/pdf-sign/util.go
@@ -1,70 +1,15 @@
 package pdf_sign
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/pem"
 	"fmt"
-	"io"
 	"os"
 	"reflect"
 
 	"crypto/x509"
 
-	pdf "github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 	"go.mozilla.org/pkcs7"
 )
-
-func WriteOutPdf(c *pdf.Context, w io.Writer) error {
-	fmt.Println(c.XRefTable.HeaderVersion)
-	c.ResetWriteContext()
-	c.Write.Eol = "\n"
-	c.Write.Writer = bufio.NewWriter(w)
-	return pdf.Write(c)
-}
-
-func GetHashRanges(c *pdf.Context, sig *pdf.Dict) [][]byte {
-	w := bytes.NewBuffer(nil)
-	WriteOutPdf(c, w)
-	b := w.Bytes()
-	ranges := make([][]byte, 0)
-	// these are fake ranges
-	ranges = append(ranges, b[0:100])
-	ranges = append(ranges, b[200:300])
-	return ranges
-}
-
-type PaddedObject struct {
-	Object pdf.Object
-	Size   int
-}
-
-func NewPaddedObject(o pdf.Array, size int) PaddedObject {
-	a := PaddedObject{Object: o}
-	a.Size = size
-	return a
-}
-
-func pad(len int) string {
-	s := ""
-	for i := 0; i < len; i++ {
-		s = s + " "
-	}
-	return s
-}
-
-/* this does not work because pdfcpu expects specific types */
-func (p PaddedObject) PDFString() string {
-	s := p.Object.PDFString()
-	if len(s) > p.Size {
-		panic("PaddedObject: object does not fit")
-	}
-	return s + pad(p.Size-len(s))
-}
-
-func (p PaddedObject) String() string {
-	return p.Object.String()
-}
 
 func Sign(hash []byte) []byte {
 	// generate a signing cert or load a key pair
